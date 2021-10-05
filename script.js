4786f99cba1c57f8b9556d8aed2b98badfb82823
marcomusa <marco.musa91@gmail.com>
2021-10-05 10:48:00 +0200

    custom navbar

---
+++ script.js
@@ -0,0 +1,16 @@
+/* EFFETTO TOGGLER */
+const navbarToggler = document.querySelector('#navbar-toggler')
+const navbarTogglerIcon = document.querySelector('#navbarToggler .fa-solid .fa-ellipsis-vertical')
+
+navbarToggler.addEventListener('click', () => {
+    navbarToggler.classList.toggle('fa-rotate-90')
+})
+
+
+/* SCROLL NAVBAR */
+const navbar = document.querySelector('.up-navbar')
+
+window.addEventListener('scroll', () => {
+
+})
+
